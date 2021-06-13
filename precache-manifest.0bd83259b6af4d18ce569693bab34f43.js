de0167b330d7399d6f6978624b586e498b83d9ec
Sohan Basak <ronnie.basak96@gmail.com>
2021-06-14 02:49:07 +0530

    update

---
+++ precache-manifest.0bd83259b6af4d18ce569693bab34f43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d2091be0da459d596c",
+    "url": "/css/chunk-vendors.4b24406e.css"
+  },
+  {
+    "revision": "2a9ce5a54a37627fe950a1bb461ec425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fb6984f04d314f81939",
+    "url": "/js/about.2a588a86.js"
+  },
+  {
+    "revision": "d9d2091be0da459d596c",
+    "url": "/js/chunk-vendors.bdf6c015.js"
+  },
+  {
+    "revision": "de0b54c4434cf0a84168",
+    "url": "/js/index.062c4f89.js"
+  },
+  {
+    "revision": "e6cb65a5d60811f80a646fc6311c5541",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
